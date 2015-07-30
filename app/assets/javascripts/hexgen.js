4fbf0e243e25a54d3a79b31b8f04cd5477f1cf12
OwenHayes <owengeorghayes@googlemail.com>
2015-07-30 13:55:39 +0100

    Challenges and Societies

---
+++ app/assets/javascripts/hexgen.js
@@ -0,0 +1,4 @@
+function hexgen(){
+ var Letter =new Array ("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
+var hexnumber = "#"+""+Letter[Math.floor((Math.random() * 16) + 0)]+""+Letter[Math.floor((Math.random() * 16) + 0)]+""+Letter[Math.floor((Math.random() * 16) + 0)]+""+Letter[Math.floor((Math.random() * 16) + 0)]+""+Letter[Math.floor((Math.random() * 16) + 0)]+""+Letter[Math.floor((Math.random() * 16) + 0)] ;
+}
